5e400824515fcb948df406d537254f55c837a960
OBouysfi <bouysfi.othman@gmail.com>
2025-12-27 14:18:51 +0100

    Added migration

--- backend/database/migrations/2025_12_27_130921_create_rendez_vous_table.php
+++ backend/database/migrations/2025_12_27_130921_create_rendez_vous_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('rendez_vous', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('display_name');
-            $table->text('description')->nullable();
-            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('rendez_vous');
     }
 };
