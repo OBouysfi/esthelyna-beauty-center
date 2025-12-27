aecf6b14c20cb403e6733e9721b4dfa6af0459c3
OBouysfi <bouysfi.othman@gmail.com>
2025-12-27 13:59:34 +0100

    Bismillah First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+http://localhost:8088
+http://localhost:3000
+http://localhost:8010
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Esthelyna Beauty Center</h1>
+    </div>
+  )
+}
